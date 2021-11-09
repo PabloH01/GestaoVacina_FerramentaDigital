45247a057b9d6d23466b39dbb294500a5165d093
PabloH01 <pablo.nadai@etec.sp.gov.br>
2021-11-09 11:26:58 -0200

    V: 4.1 Testando Banco Etim_19116

--- backend_sistema/estruturalogin/conexao.php
+++ backend_sistema/estruturalogin/conexao.php
@@ -1,9 +1,17 @@
 <?php
 
-$host = "201.62.65.6";
+/*
+$host = "localhost";
+$user = "root";
+$pass = "";
+$banco = "carteiravac";
+*/
+
+$host = "172.16.20.100";
 $user = "pablo";
 $pass = "pablo19116";
 $banco = "etim_19116";
+
 $conexao = mysqli_connect($host, $user, $pass, $banco)
                 or die("Problemas com a conexão do Banco de Dados");
                 mysqli_set_charset($conexao, "UTF8");
