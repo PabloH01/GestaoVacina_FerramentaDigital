df3cac4182d621ade7bf8f0bc83447b813c14731
PabloH01 <pablo.nadai@etec.sp.gov.br>
2021-11-09 10:25:01 -0200

    V:4 - Add banco etim_19116

--- backend_sistema/estruturalogin/conexao.php
+++ backend_sistema/estruturalogin/conexao.php
@@ -5,6 +5,12 @@ $user = "root";
 $pass = "";
 $banco = "carteiravac";
 
+/* Banco Alessandro
+$host = "172.16.20.100";
+$user = "pablo";
+$pass = "pablo19116";
+$banco = "etim_19116";
+*/
 $conexao = mysqli_connect($host, $user, $pass, $banco)
                 or die("Problemas com a conexão do Banco de Dados");
                 mysqli_set_charset($conexao, "UTF8");
